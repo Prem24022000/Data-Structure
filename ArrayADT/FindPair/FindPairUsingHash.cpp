68e87debf46aa5ddbce4f5cd51cb90bbbe22b031
prem prakash sahu <69319430+Prem24022000@users.noreply.github.com>
2021-10-11 23:55:30 +0530

    Create FindPairUsingHash.cpp

---
+++ ArrayADT/FindPair/FindPairUsingHash.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+
+int main()
+{
+ 
+    int A[10] = {6,3,8,10,16,7,5,2,9,14};
+    
+    int H[16] = {0};
+    
+    int k = 10;
+    
+    for(int i=0; i<10; i++){
+        if(H[k-A[i]] != 0 && k-A[i] >=0){
+            cout << A[i] << "+" << k-A[i] << "=" << k << endl;
+        }
+        
+        H[A[i]]++;
+    }
+    
+    return 0;
+}
